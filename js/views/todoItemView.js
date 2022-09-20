e642765d658f39eee13cae168e0d6756af8873f2
Beast2502 <mehulsaxena45@gmail.com>
2022-09-20 20:26:02 +0530

    input field capturing data

--- js/views/todoItemView.js
+++ js/views/todoItemView.js
@@ -1,33 +1,27 @@
-let TodoItemView =  Backbone.View.extend({
-    tagName : 'li',
+let TodoItemView = Backbone.View.extend({
+    tagName: "li",
+
     initialize : function(options){
         if(!options && options.model){
             throw new Error('model is not specified')
         }
 
-        this.model.on('')
+        this.model.on('change' ,this.render , this);
     },
 
-    events:{
-        'click #toggle' : 'onClickToggle'
+    events: {
+        "click #toggle" : 'onClickToggle'
     },
 
-    onClickToggle: function(){
-        console.log('Toggle clicked')
-        // console.log(this.model.get('description'))
-
-        // On way to toffle the value 
-        // this.model.set('isCompleted',!this.model.get('isCompleted'));
-
-        // another way to toggle the value by adding the method on the Model and calling it over here
+    onClickToggle : function(){
         this.model.toggle();
         console.log(this.model.toJSON());
     },
-
     render: function(){
-        // this.$el.html(this.model.get('description'));
-        this.$el.toggleClass('completed' , this.model.get('isCompleted'));
-        this.$el.html("<input id='toggle' type='checkbox'></input>"  + this.model.escape('description'));
+        this.$el.toggleClass("completed" , this.model.get("isCompleted"));
+
+        let checked = this.model.get("isCompleted" ? "checked" : "");
+        this.$el.html("<input id='toggle' type='checkbox'" + checked + "></input>" + this.model.escape("description"));
 
         return this;
     }
